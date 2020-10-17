ab26359bd4127a4cc126d2ec652f49be5fce9eee
eddie <eddie@LAPTOP-FQG2GKB5>
2020-10-17 16:46:55 +0530

    Project Initial commit

---
+++ demo/src/main/java/com/docker/application/controller/DockerController.java
@@ -0,0 +1,16 @@
+package com.docker.application.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+	
+	
+	@GetMapping("/getMessage")
+	public String getMessage() {
+		
+		return "Hi Docker Application";
+	}
+}
